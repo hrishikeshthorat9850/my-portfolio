5a38e5bdb39531b14a48e2f15ebc7352dcd7ad01
hrishikeshthorat9850 <hrishikeshthorat9850@gmail.com>
2025-08-05 21:28:12 +0530

    Updated deploy.yml

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/my-portfolio',
-  assetPrefix: '/my-portfolio/',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
